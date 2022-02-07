0349dcd37c650641f463cc4895794dfb6145a28c
BT_1E10_13 <lucyc1225xx@gmail.com>
2022-02-07 12:30:20 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> stringList7 = new ArrayList<String>(Arrays.asList("hello", "my", "best", "friend"));
+        ArrayListAlgorithms.duplicateUpperEnd(stringList7);
+        System.out.println(stringList7);
+
+
+
+
+
+
+    }
+}
